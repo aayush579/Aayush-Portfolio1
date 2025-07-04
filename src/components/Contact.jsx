e9a02370e7b16ca797650013ac297e9040ac4477
aayush579 <aayushyt980@gmail.com>
2025-07-04 08:43:51 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,8 +5,8 @@ export default function Contact() {
     <section className="p-8 max-w-3xl mx-auto text-center">
       <h2 className="text-3xl font-semibold mb-4">Contact</h2>
       <p>Email: singhaayush579@gmail.com</p>
-      <p>LinkedIn: <a className="text-blue-400 underline" href="https://linkedin.com/in/aayush579" target="_blank">linkedin.com/in/yourprofile</a></p>
-      <p>GitHub: <a className="text-blue-400 underline" href="https://github.com/aayush579" target="_blank">github.com/yourgithub</a></p>
+      <p>LinkedIn: <a className="text-blue-400 underline" href="https://linkedin.com/in/aayush579" target="_blank">linkedin.com/in</a></p>
+      <p>GitHub: <a className="text-blue-400 underline" href="https://github.com/aayush579" target="_blank">github.com</a></p>
     </section>
   );
 }
